e3311d75e770cc101a94f4930a4f862fc7fccddc
Emanuel Kluge <mail@emanuel-kluge.de>
2022-06-11 09:51:05 +0200

    feat: quit app on closing the window
    ...if this actually works...

--- Tweexel/AppDelegate.swift
+++ Tweexel/AppDelegate.swift
@@ -39,4 +39,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
